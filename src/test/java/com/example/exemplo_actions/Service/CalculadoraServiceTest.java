b005baa389f56244e736ef000e33ee6bb265c61b
Igor Nascimento <igor.silva@sptech.school>
2024-05-28 17:45:43 -0300

    Testes unitarios

---
+++ src/test/java/com/example/exemplo_actions/Service/CalculadoraServiceTest.java
@@ -0,0 +1,21 @@
+package com.example.exemplo_actions.Service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraServiceTest {
+    @Test
+    @DisplayName("Quando adicionado 10 e 2, deve retornar 5")
+    public void testDividir(){
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b= 2;
+        double esperado = 5;
+
+        double resultado = calculadoraService.dividir(a, b);
+
+        assertEquals(esperado, resultado);
+    }
+}
